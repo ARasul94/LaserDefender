9ee139b7955f6d0b70c799a7fdea5a016a8e4b11
Aliev Rasul <arasul110294@gmail.com>
2021-02-22 17:19:42 +0300

    added boundaries to player movement;

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.EventSystems;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float padding;
+    
+
+    private Vector2 _minBoundary;
+    private Vector2 _maxBoundary;
+
+    private void Start()
+    {
+        SetupBoundaries();
+    }
 
     private void Update()
     {
@@ -17,10 +23,21 @@ public class Player : MonoBehaviour
     {
         var position = transform.position;
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        var newXPos = position.x + deltaX;
+        var newXPos = Mathf.Clamp(position.x + deltaX, _minBoundary.x, _maxBoundary.x);
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * speed;
-        var newYPos = position.y + deltaY;
+        var newYPos = Mathf.Clamp(position.y + deltaY, _minBoundary.y, _maxBoundary.y);
 
         transform.position = new Vector2(newXPos, newYPos);
+    }
+    
+    private void SetupBoundaries()
+    {
+        var camera = Camera.main;
+
+        var leftBotCorner = camera.ViewportToWorldPoint(new Vector2(0, 0));
+        var rightTopCorner = camera.ViewportToWorldPoint(new Vector2(1, 1));
+
+        _minBoundary = new Vector2(leftBotCorner.x + padding, leftBotCorner.y + padding);
+        _maxBoundary = new Vector2(rightTopCorner.x - padding, rightTopCorner.y - padding);
     }
 }
