9df312b112dca2f7a52f4dac410b2b9449609ea2
Aliev Rasul <arasul110294@gmail.com>
2021-02-22 23:50:43 +0300

    added wave config scriptable object;

---
+++ Assets/Scripts/ScriptableObjects/WaveConfig.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects
+{
+    [CreateAssetMenu(menuName = "Wave Config")]
+    public class WaveConfig : ScriptableObject
+    {
+        [SerializeField] private GameObject enemy;
+        [SerializeField] private Path path;
+        [SerializeField] private float timeBetweenSpawns = 0.5f;
+        [SerializeField] private float spawnRandomFactor = 0.3f;
+        [SerializeField] private int numberOfEnemies = 5;
+        [SerializeField] private float moveSpeed = 1f;
+
+        public GameObject Enemy => enemy;
+        public Path Path => path;
+        public float TimeBetweenSpawns => timeBetweenSpawns;
+        public float SpawnRandomFactor => spawnRandomFactor;
+        public int NumberOfEnemies => numberOfEnemies;
+        public float MoveSpeed => moveSpeed;
+    }
+}
