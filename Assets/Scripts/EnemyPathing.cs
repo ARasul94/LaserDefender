3662aa1aabf7305d93fa6ef701dc273fb835d069
Aliev Rasul <arasul110294@gmail.com>
2021-02-22 23:41:08 +0300

    added logic to enemy for moving by path;

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -1,8 +1,45 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
-    [SerializeField] private List<Transform> waypoints;
+    [SerializeField] private Path path;
+    [SerializeField] private float moveSpeed;
+
+    private int _waypointIndex = 0;
+
+    private void Start()
+    {
+        if (!path.IsEmpty())
+            transform.position = path.Waypoints[_waypointIndex].position;
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (!path.IsEmpty() && path.Waypoints.Count > _waypointIndex)
+        {
+            var targetPosition = path.Waypoints[_waypointIndex].position;
+            var movementInThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(
+                transform.position, 
+                targetPosition, 
+                movementInThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                _waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 }
---
+++ Assets/Scripts/Path.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    [SerializeField] private List<Transform> waypoints;
+
+    public List<Transform> Waypoints => waypoints;
+
+    public bool IsEmpty()
+    {
+        return waypoints == null || waypoints.Count == 0;
+    }
+}
