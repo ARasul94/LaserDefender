fa1dd7893793fcf5b0545fe00a8dd953212cc3f5
Aliev Rasul <arasul110294@gmail.com>
2021-02-22 17:03:53 +0300

    added player controller script;

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var position = transform.position;
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        var newXPos = position.x + deltaX;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        var newYPos = position.y + deltaY;
+
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
