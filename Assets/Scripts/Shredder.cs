d9df5239ea202ee7f269b7222d98a3dc49fe2e20
Aliev Rasul <arasul110294@gmail.com>
2021-02-22 23:14:09 +0300

    renamed shredder prefab and script;

--- Assets/Scripts/Shredder.cs
+++ Assets/Scripts/Shredder.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shreder : MonoBehaviour
+public class Shredder : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
