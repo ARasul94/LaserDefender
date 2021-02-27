3587ca6dfed1d307c9736429d8d1781f7bf0bdaa
Aliev Rasul <arasul110294@gmail.com>
2021-02-28 02:22:40 +0300

    added music;

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour
+{
+    private void Awake()
+    {
+        var objects = FindObjectsOfType<Singleton>();
+
+        if (objects.Length > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
