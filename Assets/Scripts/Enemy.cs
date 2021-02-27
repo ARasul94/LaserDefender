592491f27a6c2cc2c20bf3dbee297bf38dc1cab7
Aliev Rasul <arasul110294@gmail.com>
2021-02-27 22:35:00 +0300

    extract process hit method;

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -10,6 +10,21 @@ public class Enemy : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         var damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        if (damageDealer == null)
+            return;
+        
         health -= damageDealer.GetDamage();
+        if (health <= 0)
+            Die();
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
     }
 }
