434054af3e9780350cbde472e88ba55f7fcd5b28
Aliev Rasul <arasul110294@gmail.com>
2021-02-27 22:50:32 +0300

    added enemy shoot;

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,11 +1,50 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal.Profiling.Memory.Experimental;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float health;
+    [SerializeField] private float shootCounter;
+    [SerializeField] private float minTimeBetweenShoots = 0.3f;
+    [SerializeField] private float maxTimeBetweenShoots = 3f;
+    [SerializeField] private GameObject laserPrefab;
+    [SerializeField] private float laserSpeed = 15;
+    
+    
+
+    private void Start()
+    {
+        SetupShootCounter();
+    }
+    
+    private void Update()
+    {
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        shootCounter -= Time.deltaTime;
+        if (shootCounter <= 0)
+            Fire();
+    }
+
+    private void Fire()
+    {
+        var laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -laserSpeed);
+        
+        SetupShootCounter();
+    }
+    
+    private void SetupShootCounter()
+    {
+        shootCounter = Random.Range(minTimeBetweenShoots, maxTimeBetweenShoots);
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
