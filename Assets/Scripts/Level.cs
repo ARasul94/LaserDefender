b2a3b4f075dae106d0208332e7b43ec6eda5ad6d
Aliev Rasul <arasul110294@gmail.com>
2021-02-28 02:12:02 +0300

    added loading game over scene if player die;

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Level : MonoBehaviour
 {
+    [SerializeField] private float loadDelay = 2f;
+    
     public void LoadStartMenuScene()
     {
         SceneManager.LoadScene("Start");
@@ -17,7 +20,18 @@ public class Level : MonoBehaviour
     
     public void LoadGameOverScene()
     {
+        StartCoroutine(LoadWithDelay(LoadGameOver));
+    }
+
+    private void LoadGameOver()
+    {
         SceneManager.LoadScene("GameOver");
+    }
+
+    private IEnumerator LoadWithDelay(Action action)
+    {
+        yield return new WaitForSeconds(loadDelay);
+        action.Invoke();
     }
 
     public void QuitGame()
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -24,7 +26,21 @@ public class Player : MonoBehaviour
     private Vector2 _minBoundary;
     private Vector2 _maxBoundary;
     private Coroutine _fireContinuously;
-    
+    private Level _level;
+    private Camera _camera;
+
+
+    private void Awake()
+    {
+        _level = FindObjectOfType<Level>();
+        if (_level == null)
+            throw new Exception($"No Level gameobject on scene {SceneManager.GetActiveScene().name}");
+        
+        _camera = Camera.main;
+        if (_camera == null)
+            throw new Exception($"No Camera gameobject on scene {SceneManager.GetActiveScene().name}");
+    }
+
     private void Start()
     {
         SetupBoundaries();
@@ -83,10 +99,8 @@ public class Player : MonoBehaviour
     
     private void SetupBoundaries()
     {
-        var camera = Camera.main;
-
-        var leftBotCorner = camera.ViewportToWorldPoint(new Vector2(0, 0));
-        var rightTopCorner = camera.ViewportToWorldPoint(new Vector2(1, 1));
+        var leftBotCorner = _camera.ViewportToWorldPoint(new Vector2(0, 0));
+        var rightTopCorner = _camera.ViewportToWorldPoint(new Vector2(1, 1));
 
         _minBoundary = new Vector2(leftBotCorner.x + padding, leftBotCorner.y + padding);
         _maxBoundary = new Vector2(rightTopCorner.x - padding, rightTopCorner.y - padding);
@@ -114,11 +128,12 @@ public class Player : MonoBehaviour
         Destroy(gameObject);
         PlaySFX(deathSound, deathSoundVolume);
         PlayVFX();
+        _level.LoadGameOverScene();
     }
 
     private void PlaySFX(AudioClip soundToPlay, float volume)
     {
-        AudioSource.PlayClipAtPoint(soundToPlay, Camera.main.transform.position, volume);
+        AudioSource.PlayClipAtPoint(soundToPlay, _camera.transform.position, volume);
     }
 
     private void PlayVFX()
